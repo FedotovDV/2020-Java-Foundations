883eb19856d7eee08a80813525e0321d84ac4ebc
Dmitriy <fed3375546@yandex.ru>
2020-01-27 22:27:25 +0300

    + rename FigureRunner

--- src/main/java/homework/figure/FigureRunner.java
+++ src/main/java/homework/figure/FigureRunner.java
@@ -2,7 +2,7 @@ package homework.figure;
 
 import java.util.ArrayList;
 //https://gist.github.com/FedotovDV/9696afc2946b74a6122a18a84f8c1ee0
-public class FigureRuner {
+public class FigureRunner {
     public static void main(String[] args) {
         Figure rectangl = new Rectangl(4.0, 3.0);
         Figure triangle1 = new Triangle(4.0, 3.0,5.0);
@@ -21,7 +21,7 @@ public class FigureRuner {
             System.out.println("Square = " + figureArrayList.get(i).calcSquare());
             System.out.println("Perimetr = " + figureArrayList.get(i).calcPerimetr());
             System.out.println();
-            System.out.println();
+
         }
 
     }
