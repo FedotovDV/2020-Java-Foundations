3fa64b2cbaf3f21b98d66cfce8148adeac06e19d
Dmitriy <fed3375546@yandex.ru>
2020-02-13 23:51:59 +0300

    + check exception in the finally block

--- src/main/java/exceptions/ExceptionRuner.java
+++ src/main/java/exceptions/ExceptionRuner.java
@@ -4,27 +4,37 @@ import interfaces.Cat;
 
 public class ExceptionRuner {
     public static void main(String[] args) {
-//
-//        Cat cat = new Cat();
-//        cat = null;
-//        cat.toString();
-//        throw new ExceptionInInitializerError();
+//        try {
+//            Cat cat = new Cat();
+//            cat = null;
+//            cat.toString();
+//        } catch (Exception exc) {
+//            throw new ExceptionInInitializerError();
+//        } finally {
+//            int x = 1;
+//            System.out.println(x = 5 / 0);
+//            System.out.println("Finally 1st try");
+//        }
+        /*
+        Exception in thread "main" java.lang.ArithmeticException: / by zero
+        at exceptions.ExceptionRuner.main(ExceptionRuner.java:15)
+        */
 
         Man alexandr = new Man("Alexandr", 10);
         try {
             alexandr.setAge(-5);
-        } catch(MyNewException exc){
+        } catch (MyNewException exc) {
             System.err.println("Bad age ");
             exc.getMessage();
             exc.printStackTrace();
-        } finally{
+        } finally {
             System.out.println("Finally 1st try");
         }
 
         Woman mariya = new Woman("Mariya", 30);
-        try{
+        try {
             mariya.setWeight(105);
-        } catch(MyNewRuntimeException re){
+        } catch (MyNewRuntimeException re) {
             re.printStackTrace();
         }
     }
