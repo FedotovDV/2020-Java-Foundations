38491587cd08ab67fea4326c17cc47e772df2280
Dmitriy <fed3375546@yandex.ru>
2020-03-08 13:51:23 +0300

    homework save text in file

--- src/main/java/homework/iostreams/SaveText.java
+++ src/main/java/homework/iostreams/SaveText.java
@@ -10,9 +10,8 @@ public class SaveText {
 
     public static void main(String[] args) {
 
-        File file = new File("\\") ;
+        File file = new File("\\");
         List<String> text = new ArrayList<>();
-
         file = inputFileAndText(file, text);
         outputTextFile(file, text);
 
@@ -20,36 +19,45 @@ public class SaveText {
     }
 
     private static File inputFileAndText(File file, List<String> text) {
-        String fileName;
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Введите имя файла: ");
-            fileName = bufferedReader.readLine();
-            System.out.println("Введите путь: ");
-            file = new File(bufferedReader.readLine() + "\\" + fileName);
-            System.out.println("file = " + file);
-            System.out.println("Введите текст: (и Exit для завершения)");
-            String input;
-            int count = 0;
-            while ((count++ < 10) && !(input = bufferedReader.readLine()).equals("Exit")) {
-                text.add(input);
-            }
+            file = inputFile(bufferedReader);
+            inputText(file, text, bufferedReader);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return file;
     }
 
-    private static void outputTextFile(File file, List<String> text) {
-
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-                for (String string : text) {
-                    bufferedWriter.write(string+ "\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private static File inputFile(BufferedReader bufferedReader) throws IOException {
+        String fileName;
+        System.out.println("Введите имя файла: ");
+        fileName = bufferedReader.readLine();
+        System.out.println("Введите путь: ");
+        File file = new File(bufferedReader.readLine() + "\\" + fileName);
+        System.out.println("file = " + file);
+        return file;
     }
 
+    private static List<String> inputText(File file, List<String> text, BufferedReader bufferedReader) throws IOException {
+        System.out.println("Введите текст: (и Exit для завершения)");
+        String input;
+        int count = 0;
+        while ((count++ < 10) && !(input = bufferedReader.readLine()).equals("Exit")) {
+            text.add(input);
+        }
+        return text;
+    }
+
+    private static void outputTextFile(File file, List<String> text) {
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            for (String string : text) {
+                bufferedWriter.write(string + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
