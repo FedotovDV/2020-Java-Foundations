0f1886a230e7b77e0e554fca278bb104eab46101
Dmitriy <fed3375546@yandex.ru>
2020-03-13 19:41:36 +0300

    replace the switch with a map

--- src/main/java/pattern/factory/BeanFactory.java
+++ src/main/java/pattern/factory/BeanFactory.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class BeanFactory {
 
     static Map<BeanType, Bean> beanTypeMap = new HashMap<>();
+
     static {
         beanTypeMap.put(BeanType.APPLE, new AppleFlavourBean());
         beanTypeMap.put(BeanType.CHOCOLATE, new ChocolateFlavourBean());
@@ -19,34 +20,9 @@ public class BeanFactory {
         beanTypeMap.put(BeanType.SPINACH, new SpinachFlavourBean());
         beanTypeMap.put(BeanType.SPROUTS, new SproutsFlavourBean());
         beanTypeMap.put(BeanType.STRAWBERRY, new StrawberryFlavourBean());
-
     }
 
     public Bean getBean(BeanType beanType) {
-
-        switch (beanType) {
-            case APPLE:
-                return new AppleFlavourBean();
-            case CHOCOLATE:
-                return new ChocolateFlavourBean();
-            case GRASS:
-                return new GrassFlavourBean();
-            case LIVER:
-                return new LiverFlavourBean();
-            case SOAP:
-                return new SoapFlavourBean();
-            case SOCK:
-                return new SockFlavourBean();
-            case SPINACH:
-                return new SpinachFlavourBean();
-            case SPROUTS:
-                return new SproutsFlavourBean();
-            case STRAWBERRY:
-                return new StrawberryFlavourBean();
-            default:
-                new IllegalArgumentException();
-        }
-        return null;
-
+        return beanTypeMap.get(beanType);
     }
 }
--- src/main/java/pattern/factory/Main.java
+++ src/main/java/pattern/factory/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         BeanFactory beanFactory = new BeanFactory();
         eatRandomBean(beanFactory);
+
     }
 
     private static void eatRandomBean(BeanFactory beanFactory) {
