7d494af83dcee3429f476e0f2a4b57896356f99a
Dmitriy <fed3375546@yandex.ru>
2020-03-13 19:59:18 +0300

    homework factory pattern

--- src/main/java/pattern/factory/BeanFactory.java
+++ src/main/java/pattern/factory/BeanFactory.java
@@ -25,4 +25,42 @@ public class BeanFactory {
     public Bean getBean(BeanType beanType) {
         return beanTypeMap.get(beanType);
     }
+
+//
+//    public Bean getBean(BeanType beanType) {
+//        Bean youBean = null;
+//        switch (beanType) {
+//            case APPLE:
+//                youBean = new AppleFlavourBean();
+//                break;
+//            case CHOCOLATE:
+//                youBean = new ChocolateFlavourBean();
+//                break;
+//            case GRASS:
+//                youBean = new GrassFlavourBean();
+//                break;
+//            case LIVER:
+//                youBean = new LiverFlavourBean();
+//                break;
+//            case SOAP:
+//                youBean = new SoapFlavourBean();
+//                break;
+//            case SOCK:
+//                youBean = new SockFlavourBean();
+//                break;
+//            case SPINACH:
+//                youBean = new SpinachFlavourBean();
+//                break;
+//            case SPROUTS:
+//                youBean = new SproutsFlavourBean();
+//                break;
+//            case STRAWBERRY:
+//                youBean = new StrawberryFlavourBean();
+//                break;
+////            default:
+////                new IllegalArgumentException();
+//        }
+//        return youBean;
+//
+//    }
 }
