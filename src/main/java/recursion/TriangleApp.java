f063c8b28af7236a1917b47ff4430a0689b38b63
Dmitriy <fed3375546@yandex.ru>
2020-05-03 02:43:58 +0300

    recursion practice with @Log4j and @Test

---
+++ src/main/java/recursion/TriangleApp.java
@@ -0,0 +1,21 @@
+package recursion;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+public class TriangleApp {
+
+    public static int triangle(int n) {
+        log.info("Entering: n=" + n);
+        if (n == 1) {
+            log.info("Returning 1");
+            return 1;
+        }
+        int temp = n + triangle(n - 1);
+        log.info("Returning:" + temp);
+        return temp;
+    }
+
+
+}
---
+++ src/test/recursion/TriangleAppTest.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static recursion.TriangleApp.triangle;
+
+public class TriangleAppTest {
+
+    @Test
+    public void testTriangle() {
+        int theNumber1 = 1;
+        int theNumber2 = 5;
+        assertThat(triangle(theNumber1)).isEqualTo(1);
+        assertThat(triangle(theNumber2)).isEqualTo(((theNumber2 + 1) / 2) * theNumber2);
+    }
+}
