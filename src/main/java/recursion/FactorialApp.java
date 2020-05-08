2199de538c708db331ab144f5b79ef00f36e0b8c
Dmitriy <fed3375546@yandex.ru>
2020-05-08 22:57:32 +0300

    recursion practice with junit test

---
+++ src/main/java/recursion/FactorialApp.java
@@ -0,0 +1,18 @@
+package recursion;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class FactorialApp {
+    public static int factorial(int n) {
+        log.info("Entering: n=" + n);
+        if (n == 0) {
+            log.info("Returning 1");
+            return 1;
+        }
+        int temp = n * factorial(n - 1);
+        log.info("Returning:" + temp);
+        return temp;
+    }
+
+
+}
---
+++ src/test/recursion/FactorialAppTest.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static recursion.FactorialApp.factorial;
+
+public class FactorialAppTest {
+
+    @Test
+    public void testFactorial() {
+        int theNumber1 = 0;
+        int theNumber2 = 6;
+        assertEquals(factorial(theNumber1), 1);
+        assertEquals(factorial(theNumber2), 720);
+    }
+}
